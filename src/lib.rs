14a4a427c48947e84978b33e401e0151326a14ff
gwmcassella <gwmcassella@gmail.com>
2022-11-30 23:11:20 +0000

    Reimplement fft using num_complex::Complex

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 //! As many classic numerical physics methods as I can remember.
 #![deny(missing_docs)]
-#![deny(rustdoc::missing_doc_code_examples)]
 
 /// Helper module for handling data
 pub mod data;
@@ -94,7 +93,6 @@ pub mod integrate {
         ta = tb;
         yb = self.step(ta, &ya).unwrap();
         tb = self.h + ta;
-        println!("{} {} {} {}", ta, ya[(0)], tb, yb[(0)]);
       } // After this loop, [ya, yb] straddle the solution and ta < t1 < tb.
 
       // Construct Hermite interpolant and find value at t1
@@ -245,9 +243,21 @@ pub mod interpolate {
 
 /// A crusty Fast Fourier Transform module
 pub mod fft {
+  extern crate num_complex;
+  use num_complex::Complex;
+
+  /// Convert a vector of real numbers represented by f64 to a vector of
+  /// the same numbers represented as Complex<f64>.
+  ///
+  /// This can be made faster using unsafe code but I am not confident enough
+  /// to do so just yet, see
+  /// https://stackoverflow.com/questions/54185667/how-to-safely-reinterpret-vecf64-as-vecnum-complexcomplexf64-with-half-t
+  fn vec_real_to_complex(v: &Vec<f64>) -> Vec<Complex<f64>> {
+    v.iter().map(|x| Complex::new(*x, 0.0)).collect()
+  }
+
   /// Compute the discrete Fourier transform of `x` using the Danielson-Lanczos
-  /// algorithm. Assume data is given as a length `2n` vector of `n` complex
-  /// numbers.
+  /// algorithm. Algorithm is carried out in-place.
   ///
   /// Algorithm and code obtained from Numerical Recipes in C, 3rd Edition.
   ///
@@ -256,98 +266,195 @@ pub mod fft {
   /// ```
   /// use crusty::fft::fft;
   /// use assert_approx_eq::assert_approx_eq;
+  /// use num_complex::Complex;
   ///
-  /// let x = vec![1.0, 0.0, 2.0, 0.0, 3.0, 0.0, 4.0, 0.0];
+  /// let x = vec![Complex::new(1.0, 0.0), Complex::new(2.0, 0.0), Complex::new(3.0, 0.0), Complex::new(4.0, 0.0)];
   /// let y = fft(x, -1).unwrap();
-  /// assert_approx_eq!(y[0], 10.0, 1e-6);
-  /// assert_approx_eq!(y[1], 0.0, 1e-6);
-  /// assert_approx_eq!(y[2], -2.0, 1e-6);
-  /// assert_approx_eq!(y[3], 2.0, 1e-6);
-  /// assert_approx_eq!(y[4], -2.0, 1e-6);
-  /// assert_approx_eq!(y[5], 0.0, 1e-6);
-  /// assert_approx_eq!(y[6], -2.0, 1e-6);
-  /// assert_approx_eq!(y[7], -2.0, 1e-6);
+  /// println!("{:?}", y);
+  /// assert_approx_eq!(y[0].re, 10.0, 1e-6);
+  /// assert_approx_eq!(y[0].im, 0.0, 1e-6);
+  /// assert_approx_eq!(y[1].re, -2.0, 1e-6);
+  /// assert_approx_eq!(y[1].im, 2.0, 1e-6);
+  /// assert_approx_eq!(y[2].re, -2.0, 1e-6);
+  /// assert_approx_eq!(y[2].im, 0.0, 1e-6);
+  /// assert_approx_eq!(y[3].re, -2.0, 1e-6);
+  /// assert_approx_eq!(y[3].im, -2.0, 1e-6);
   /// ```
-  pub fn fft(mut data: Vec<f64>, isign: i32) -> Result<Vec<f64>, String> {
-    let n = data.len() / 2 as usize;
-    if data.len() % 2 != 0 {
-      return Err(String::from("data length is not a multiple of 2"));
-    }
+  pub fn fft(
+    mut data: Vec<Complex<f64>>,
+    isign: i32,
+  ) -> Result<Vec<Complex<f64>>, String> {
+    let n = data.len() as usize;
     if n < 2 || n & (n - 1) != 0 {
       return Err(String::from("data length is not a power of 2"));
     }
 
-    let nn = 2 * n as usize;
-    let mut mmax: usize = 2;
-    let mut j: usize = 1;
-    for i in (1..nn).step_by(2) {
-      if j > i as usize {
-        data.swap(j - 1 as usize, i as usize - 1);
-        data.swap(i as usize, j as usize);
+    let mut j = 0;
+    for i in 0..n - 1 {
+      if j > i {
+        data.swap(i, j);
       }
-
-      let mut m = n;
-      while m >= 2 && j > m {
+      let mut m = n >> 1; // Set m to 1 in bit-reversed binary
+      while m != 0 && j >= m {
+        // From left to right in the binary representation of j, find the first
+        // 0. On the way, set all 1s to 0s, and then set the first 0 to 1. This
+        // procedure increments j by 1 in bit-reversed binary counting.
         j -= m;
-        m /= 2;
+        m >>= 1;
       }
-      j += m;
+      j += m; // set first 0 to 1
     }
 
-    while nn > mmax {
-      let istep = 2 * mmax;
-      let theta = isign as f64 * (2.0 * std::f64::consts::PI / mmax as f64);
+    let mut m = 2;
+    while n >= m {
+      let theta = isign as f64 * (2.0 * std::f64::consts::PI / m as f64);
       let wtemp = (0.5 * theta).sin();
-      let wpr = -2.0 * wtemp * wtemp;
-      let wpi = (theta).sin();
-      let mut wr = 1.0;
-      let mut wi = 0.0;
-      for m in (1..mmax).step_by(2) {
-        for i in (m..nn).step_by(istep) {
-          let j = i as usize + mmax;
-          let tempr = wr * data[j - 1] - wi * data[j];
-          let tempi = wr * data[j] + wi * data[j - 1];
-          data[j - 1] = data[i as usize - 1] - tempr;
-          data[j] = data[i as usize] - tempi;
-          data[i as usize - 1] += tempr;
-          data[i as usize] += tempi;
+      let wm = Complex::new(-2.0 * wtemp * wtemp, (theta).sin());
+
+      for k in (0..n).step_by(m) {
+        let mut w = Complex::new(1.0, 0.0);
+        for j in 0..m / 2 {
+          let t = w * data[k + j + m / 2];
+          let u = data[k + j];
+          data[k + j] = u + t;
+          data[k + j + m / 2] = u - t;
+          w += wm * w;
         }
-        let wtemp = wr;
-        wr += wr * wpr - wi * wpi;
-        wi += wi * wpr + wtemp * wpi;
       }
-      mmax = istep;
+
+      m <<= 1;
     }
     Ok(data)
   }
 
-  /// Compute the discrete Fourier transform of a vector of `n` real values
+  /// Returns `crusty::fft:fft(x.clone())`
   ///
-  /// `isign = -1` for forward transform, `isign = 1` for inverse transform.
   /// ```
-  /// use crusty::fft::rfft;
+  /// use crusty::fft::fft_clone;
   /// use assert_approx_eq::assert_approx_eq;
-  /// let x = vec![1.0, 2.0, 3.0, 4.0];
-  /// let y = rfft(x, -1).unwrap();
-  /// assert_approx_eq!(y[0], 10.0, 1e-6);
-  /// assert_approx_eq!(y[1], 0.0, 1e-6);
-  /// assert_approx_eq!(y[2], -2.0, 1e-6);
-  /// assert_approx_eq!(y[3], 2.0, 1e-6);
-  /// assert_approx_eq!(y[4], -2.0, 1e-6);
-  /// assert_approx_eq!(y[5], 0.0, 1e-6);
-  /// assert_approx_eq!(y[6], -2.0, 1e-6);
-  /// assert_approx_eq!(y[7], -2.0, 1e-6);
+  /// use num_complex::Complex;
+  ///
+  /// let x = vec![Complex::new(1.0, 0.0), Complex::new(2.0, 0.0), Complex::new(3.0, 0.0), Complex::new(4.0, 0.0)];
+  /// let y = fft_clone(&x, -1).unwrap();
+  /// assert_approx_eq!(y[0].re, 10.0, 1e-6);
+  /// assert_approx_eq!(y[0].im, 0.0, 1e-6);
+  /// assert_approx_eq!(y[1].re, -2.0, 1e-6);
+  /// assert_approx_eq!(y[1].im, 2.0, 1e-6);
+  /// assert_approx_eq!(y[2].re, -2.0, 1e-6);
+  /// assert_approx_eq!(y[2].im, 0.0, 1e-6);
+  /// assert_approx_eq!(y[3].re, -2.0, 1e-6);
+  /// assert_approx_eq!(y[3].im, -2.0, 1e-6);
+  /// assert_approx_eq!(x[0].re, 1.0, 1e-6);
   /// ```
-  pub fn rfft(mut data: Vec<f64>, isign: i32) -> Result<Vec<f64>, String> {
-    let n = data.len();
-    data.resize(n * 2, 0.0);
-    for i in (0..n).rev() {
-      data[i * 2] = data[i];
-      data[i * 2 + 1] = 0.0;
-    }
-    fft(data, isign)
+  pub fn fft_clone(
+    x: &Vec<Complex<f64>>,
+    isign: i32,
+  ) -> Result<Vec<Complex<f64>>, String> {
+    let y = x.clone();
+    fft(y, isign)
+  }
+
+  /// Returns `crusty::fft:fft(y)`, where `y` is the Vector<Complex<f64>>
+  /// representation of the Vector<f64> `x`.
+  ///
+  /// ```
+  /// use crusty::fft::rfft_clone;
+  /// use assert_approx_eq::assert_approx_eq;
+  /// use num_complex::Complex;
+  ///
+  /// let x = vec![1.0, 2.0, 3.0, 4.0];
+  /// let y = rfft_clone(&x, -1).unwrap();
+  /// assert_approx_eq!(y[0].re, 10.0, 1e-6);
+  /// assert_approx_eq!(y[0].im, 0.0, 1e-6);
+  /// assert_approx_eq!(y[1].re, -2.0, 1e-6);
+  /// assert_approx_eq!(y[1].im, 2.0, 1e-6);
+  /// assert_approx_eq!(y[2].re, -2.0, 1e-6);
+  /// assert_approx_eq!(y[2].im, 0.0, 1e-6);
+  /// assert_approx_eq!(y[3].re, -2.0, 1e-6);
+  /// assert_approx_eq!(y[3].im, -2.0, 1e-6);
+  /// assert_approx_eq!(x[0], 1.0, 1e-6);
+  /// ```
+  pub fn rfft_clone(
+    x: &Vec<f64>,
+    isign: i32,
+  ) -> Result<Vec<Complex<f64>>, String> {
+    let y = vec_real_to_complex(x);
+    fft(y, isign)
   }
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+  /// Tests `crusty::fft::fft` against precomputed fft of a random array
+  /// using Python+numpy
+  #[test]
+  fn test_fft_big_vec() {
+    use super::fft::rfft_clone;
+    use assert_approx_eq::assert_approx_eq;
+
+    let x = vec![
+      0.36117797, 0.75825683, 0.00786384, 0.16040471, 0.40329213, 0.22808298,
+      0.32710224, 0.8247796, 0.85168761, 0.05081063, 0.63342277, 0.10148653,
+      0.82063687, 0.12473368, 0.3725931, 0.28827866,
+    ];
+
+    let y = rfft_clone(&x, -1).unwrap();
+    assert_approx_eq!(y[0].re, 6.31461013, 1e-6);
+    assert_approx_eq!(y[1].re, -0.75974994, 1e-6);
+    assert_approx_eq!(y[2].re, 0.91342059, 1e-6);
+    assert_approx_eq!(y[3].re, 0.02612736, 1e-6);
+    assert_approx_eq!(y[4].re, 1.09581263, 1e-6);
+    assert_approx_eq!(y[5].re, -0.18680651, 1e-6);
+    assert_approx_eq!(y[6].re, -0.93554742, 1e-6);
+    assert_approx_eq!(y[7].re, -1.04160947, 1e-6);
+    assert_approx_eq!(y[8].re, 1.24094292, 1e-6);
+    assert_approx_eq!(y[9].re, -1.04160947, 1e-6);
+    assert_approx_eq!(y[10].re, -0.93554742, 1e-6);
+    assert_approx_eq!(y[11].re, -0.18680651, 1e-6);
+    assert_approx_eq!(y[12].re, 1.09581263, 1e-6);
+    assert_approx_eq!(y[13].re, 0.02612736, 1e-6);
+    assert_approx_eq!(y[14].re, 0.91342059, 1e-6);
+    assert_approx_eq!(y[15].re, -0.75974994, 1e-6);
+
+    assert_approx_eq!(y[0].im, 0., 1e-6);
+    assert_approx_eq!(y[1].im, 0.26589503, 1e-6);
+    assert_approx_eq!(y[2].im, 0.33765665, 1e-6);
+    assert_approx_eq!(y[3].im, -1.03000111, 1e-6);
+    assert_approx_eq!(y[4].im, 0.21306537, 1e-6);
+    assert_approx_eq!(y[5].im, -1.14431934, 1e-6);
+    assert_approx_eq!(y[6].im, 0.22083921, 1e-6);
+    assert_approx_eq!(y[7].im, -1.51780216, 1e-6);
+    assert_approx_eq!(y[8].im, 0., 1e-6);
+    assert_approx_eq!(y[9].im, 1.51780216, 1e-6);
+    assert_approx_eq!(y[10].im, -0.22083921, 1e-6);
+    assert_approx_eq!(y[11].im, 1.14431934, 1e-6);
+    assert_approx_eq!(y[12].im, -0.21306537, 1e-6);
+    assert_approx_eq!(y[13].im, 1.03000111, 1e-6);
+    assert_approx_eq!(y[14].im, -0.33765665, 1e-6);
+    assert_approx_eq!(y[15].im, -0.26589503, 1e-6);
+  }
+
+  /// Tests `crusty::fft::fft` against a purely imaginary input
+  #[test]
+  fn test_fft_pure_imag() {
+    use super::fft::fft;
+    use assert_approx_eq::assert_approx_eq;
+    use num_complex::Complex;
+    let x = vec![
+      Complex::new(0.0, 1.0),
+      Complex::new(0.0, 2.0),
+      Complex::new(0.0, 3.0),
+      Complex::new(0.0, 4.0),
+    ];
+    let y = fft(x, -1).unwrap();
+    println!("{:?}", y);
+    assert_approx_eq!(y[0].re, 0.0, 1e-6);
+    assert_approx_eq!(y[0].im, 10.0, 1e-6);
+    assert_approx_eq!(y[1].re, -2.0, 1e-6);
+    assert_approx_eq!(y[1].im, -2.0, 1e-6);
+    assert_approx_eq!(y[2].re, 0.0, 1e-6);
+    assert_approx_eq!(y[2].im, -2.0, 1e-6);
+    assert_approx_eq!(y[3].re, 2.0, 1e-6);
+    assert_approx_eq!(y[3].im, -2.0, 1e-6);
+  }
+}
